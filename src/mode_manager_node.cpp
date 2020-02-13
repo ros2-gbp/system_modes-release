1085b3e004b6f5c3edeafcfd009954d1fe6a18cc
Arne Nordmann <arne.nordmann@de.bosch.com>
2020-02-13 09:26:59 +0100

    Rebase from 'release/eloquent/system_modes'

--- src/mode_manager_node.cpp
+++ src/mode_manager_node.cpp
@@ -28,28 +28,24 @@
 #include <utility>
 #include <iostream>
 
-#include "system_modes/mode_monitor.hpp"
+#include "system_modes/mode_manager.hpp"
 
 using std::cerr;
 using std::cout;
-using std::endl;
 using std::string;
 using std::vector;
 using std::function;
-using std::exception;
 using std::make_pair;
 using std::shared_ptr;
-using std::make_shared;
-using std::invalid_argument;
 
-using system_modes::ModeMonitor;
+using system_modes::ModeManager;
 using system_modes::DEFAULT_MODE;
 using system_modes::msg::ModeEvent;
+
 using lifecycle_msgs::msg::State;
 using lifecycle_msgs::msg::TransitionEvent;
 
 using boost::program_options::value;
-using boost::program_options::bool_switch;
 using boost::program_options::variables_map;
 using boost::program_options::command_line_parser;
 using boost::program_options::options_description;
@@ -59,30 +55,26 @@ using rcl_interfaces::msg::ParameterType;
 using rcl_interfaces::msg::ParameterEvent;
 
 string modelfile, loglevel;
-bool debug = false;
-unsigned int rate = 1000;
-bool verbose = false;
 options_description options("Allowed options");
 
-shared_ptr<system_modes::ModeMonitor> monitor;
+shared_ptr<ModeManager> manager;
 
 bool parseOptions(int argc, char * argv[])
 {
-  options.add_options()("help", "Help message and options")("modelfile",
-    value<string>(&modelfile), "Path to yaml model file")("__log_level",
-    value<string>(&loglevel), "ROS 2 log level")
-    ("debug,d", bool_switch(&debug)->default_value(false), "Debug mode (don't clear screen)")
-    ("rate,r", value<unsigned int>(&rate)->default_value(1000), "Update rate in milliseconds")
-    ("verbose,v", bool_switch(&verbose)->default_value(false), "Verbose (displays mode parameters)");
+  options.add_options()("help", "Help message and options")(
+    "modelfile", value<string>(&modelfile),
+    "Path to yaml model file")(
+    "__log_level", value<string>(&loglevel),
+    "ROS2 log level")(
+    "ros-args", value<vector<string>>()->multitoken(), "ROS args")(
+    "params-file", value<vector<string>>()->multitoken(), "ROS params file");
 
   positional_options_description positional_options;
   positional_options.add("modelfile", 1);
-  positional_options.add("debug", 0);
-  positional_options.add("rate", 1);
-  positional_options.add("verbose", 0);
 
   variables_map map;
-  store(command_line_parser(argc, argv)
+  store(
+    command_line_parser(argc, argv)
     .options(options)
     .positional(positional_options)
     .run(), map);
@@ -91,10 +83,6 @@ bool parseOptions(int argc, char * argv[])
   if (map.count("help")) {
     return true;
   }
-
-  if (modelfile.empty()) {
-    throw invalid_argument("Need path to model file.");
-  }
   return false;
 }
 
@@ -102,15 +90,15 @@ void transition_callback(
   const TransitionEvent::SharedPtr msg,
   const string & node_name)
 {
-  monitor->inference()->update_state(node_name, msg->goal_state.id);
+  manager->inference()->update_state(node_name, msg->goal_state.id);
 }
 
 void mode_change_callback(
   const ModeEvent::SharedPtr msg,
   const string & node_name)
 {
-  monitor->inference()->update_state(node_name, State::PRIMARY_STATE_ACTIVE);
-  monitor->inference()->update_mode(node_name, msg->goal_mode.label.c_str());
+  manager->inference()->update_state(node_name, State::PRIMARY_STATE_ACTIVE);
+  manager->inference()->update_mode(node_name, msg->goal_mode.label.c_str());
 }
 
 void transition_request_callback(
@@ -118,10 +106,12 @@ void transition_request_callback(
   const string & node_name)
 {
   if (msg->goal_state.id != State::PRIMARY_STATE_ACTIVE) {
-    monitor->inference()->update_target(node_name,
+    manager->inference()->update_target(
+      node_name,
       make_pair(msg->goal_state.id, ""));
   } else {
-    monitor->inference()->update_target(node_name,
+    manager->inference()->update_target(
+      node_name,
       make_pair(msg->goal_state.id, DEFAULT_MODE));
   }
 }
@@ -130,7 +120,8 @@ void mode_request_callback(
   const ModeEvent::SharedPtr msg,
   const string & node_name)
 {
-  monitor->inference()->update_target(node_name,
+  manager->inference()->update_target(
+    node_name,
     make_pair(State::PRIMARY_STATE_ACTIVE, msg->goal_mode.label.c_str()));
 }
 
@@ -139,15 +130,15 @@ parameter_event_callback(const ParameterEvent::SharedPtr event)
 {
   for (auto p : event->new_parameters) {
     auto param = rclcpp::Parameter::from_parameter_msg(p);
-    monitor->inference()->update_param(event->node, param);
+    manager->inference()->update_param(event->node, param);
   }
   for (auto p : event->changed_parameters) {
     auto param = rclcpp::Parameter::from_parameter_msg(p);
-    monitor->inference()->update_param(event->node, param);
+    manager->inference()->update_param(event->node, param);
   }
   for (auto p : event->deleted_parameters) {
     auto param = rclcpp::Parameter::from_parameter_msg(p);
-    monitor->inference()->update_param(event->node, param);
+    manager->inference()->update_param(event->node, param);
   }
 }
 
@@ -158,21 +149,23 @@ int main(int argc, char * argv[])
   // Handle commandline arguments.
   try {
     if (parseOptions(argc, argv)) {
-      cout << "Usage: mode-monitor MODELFILE" << endl;
-      cout << options << endl;
+      cout << "Usage: mode_manager MODELFILE" << std::endl;
+      cout << options;
+      cout << "Or specify the MODELFILE by ROS parameter 'modelfile'." << std::endl << std::endl;
       return EXIT_SUCCESS;
     }
-  } catch (const exception & e) {
-    cerr << "Error parsing command line: " << e.what() << endl;
-    cout << "Usage: mode-monitor MODELFILE" << endl;
-    cout << options << endl;
+  } catch (const std::exception & e) {
+    cerr << "Error parsing command line: " << e.what() << std::endl;
+    cout << "Usage: mode_manager [MODELFILE]" << std::endl;
+    cout << options;
+    cout << "Or specify the MODELFILE by ROS parameter 'modelfile'." << std::endl << std::endl;
     return EXIT_FAILURE;
   }
 
   setvbuf(stdout, NULL, _IONBF, BUFSIZ);
   rclcpp::init(argc, argv);
 
-  monitor = make_shared<ModeMonitor>(modelfile, rate, verbose, !debug);
+  manager = std::make_shared<ModeManager>(modelfile);
 
   vector<shared_ptr<rclcpp::Subscription<TransitionEvent>>>
   state_sub_;
@@ -184,7 +177,7 @@ int main(int argc, char * argv[])
   mode_request_sub_;
 
   // Subscribe to lifecycle messages of each node
-  for (auto node : monitor->inference()->get_nodes()) {
+  for (auto node : manager->inference()->get_nodes()) {
     string lifecycle_topic = "/" + node + "/transition_event";
     string mode_topic = "/" + node + "/mode_event";
 
@@ -194,7 +187,7 @@ int main(int argc, char * argv[])
     // Callback for lifecycle transitions
     function<void(TransitionEvent::SharedPtr)> state_callback =
       bind(transition_callback, _1, node);
-    auto state_sub = monitor->create_subscription<TransitionEvent>(
+    auto state_sub = manager->create_subscription<TransitionEvent>(
       lifecycle_topic,
       rclcpp::SystemDefaultsQoS(),
       state_callback);
@@ -203,15 +196,16 @@ int main(int argc, char * argv[])
     // Callback for mode transitions
     function<void(ModeEvent::SharedPtr)> mode_callback =
       bind(mode_change_callback, _1, node);
-    auto mode_sub = monitor->create_subscription<ModeEvent>(mode_topic,
-        rclcpp::SystemDefaultsQoS(),
-        mode_callback);
+    auto mode_sub = manager->create_subscription<ModeEvent>(
+      mode_topic,
+      rclcpp::SystemDefaultsQoS(),
+      mode_callback);
     mode_sub_.push_back(mode_sub);
 
     // Callback for lifecycle transitions request info
     state_callback =
       bind(transition_request_callback, _1, node);
-    state_sub = monitor->create_subscription<TransitionEvent>(
+    state_sub = manager->create_subscription<TransitionEvent>(
       lifecycle_request_topic,
       rclcpp::SystemDefaultsQoS(),
       state_callback);
@@ -220,21 +214,22 @@ int main(int argc, char * argv[])
     // Callback for mode transitions request info
     mode_callback =
       bind(mode_request_callback, _1, node);
-    mode_sub = monitor->create_subscription<ModeEvent>(mode_request_topic,
-        rclcpp::SystemDefaultsQoS(),
-        mode_callback);
+    mode_sub = manager->create_subscription<ModeEvent>(
+      mode_request_topic,
+      rclcpp::SystemDefaultsQoS(),
+      mode_callback);
     mode_request_sub_.push_back(mode_sub);
   }
 
   // Subscribe to lifecycle messages of each system
-  for (auto system : monitor->inference()->get_systems()) {
+  for (auto system : manager->inference()->get_systems()) {
     string lifecycle_request_topic = "/" + system + "/transition_request_info";
     string mode_request_topic = "/" + system + "/mode_request_info";
 
     // Callback for lifecycle transitions request info
     function<void(TransitionEvent::SharedPtr)> state_callback =
       bind(transition_request_callback, _1, system);
-    auto state_sub = monitor->create_subscription<TransitionEvent>(
+    auto state_sub = manager->create_subscription<TransitionEvent>(
       lifecycle_request_topic,
       rclcpp::SystemDefaultsQoS(),
       state_callback);
@@ -243,19 +238,21 @@ int main(int argc, char * argv[])
     // Callback for mode transitions request info
     function<void(ModeEvent::SharedPtr)> mode_callback =
       bind(mode_request_callback, _1, system);
-    auto mode_sub = monitor->create_subscription<ModeEvent>(mode_request_topic,
-        rclcpp::SystemDefaultsQoS(),
-        mode_callback);
+    auto mode_sub = manager->create_subscription<ModeEvent>(
+      mode_request_topic,
+      rclcpp::SystemDefaultsQoS(),
+      mode_callback);
     mode_request_sub_.push_back(mode_sub);
   }
 
   // Listen for parameter changes
-  auto param_sub = monitor->create_subscription<ParameterEvent>("/parameter_events",
-      rclcpp::SystemDefaultsQoS(),
-      parameter_event_callback);
+  auto param_sub = manager->create_subscription<ParameterEvent>(
+    "/parameter_events",
+    rclcpp::ParameterEventsQoS(),
+    parameter_event_callback);
 
   rclcpp::executors::SingleThreadedExecutor exe;
-  exe.add_node(monitor);
+  exe.add_node(manager);
   exe.spin();
 
   rclcpp::shutdown();
