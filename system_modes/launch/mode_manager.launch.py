49b4ee39295cd2f22a8b97c4cd4ac8f4b89abcb0
Nordmann Arne (CR/ADT3) <arne.nordmann@de.bosch.com>
2021-07-21 12:57:59 +0200

    Revert "Trimmed the branch to only the system_modes_msgs sub directory"
    This reverts commit 5929846eb70a29ba8065fa91b367cbf301f906ce.

---
+++ system_modes/launch/mode_manager.launch.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2019 - for information on the respective copyright owner
+# see the NOTICE file and/or the repository https://github.com/micro-ROS/system_modes.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import launch
+import launch.actions
+import launch.substitutions
+
+import launch_ros.actions
+
+
+def generate_launch_description():
+    return launch.LaunchDescription([
+        launch.actions.DeclareLaunchArgument(
+            'modelfile',
+            description='Path to modelfile'),
+        launch_ros.actions.Node(
+            package='system_modes',
+            executable='mode_manager',
+            parameters=[{'modelfile': launch.substitutions.LaunchConfiguration('modelfile')}],
+            output='screen')])
